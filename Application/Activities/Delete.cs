5e1a9f0920b8b40a7878df0b76582db40739b5ab
Vsevolod <davydovvy@gmail.com>
2020-02-02 00:34:32 +0300

    End of section 10

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -6,36 +6,36 @@ using Persistence;
 
 namespace Application.Activities
 {
-    public class Delete
-    {
-        public class Command : IRequest
-        {
-            public Guid Id { get; set; }
-        }
+   public class Delete
+   {
+      public class Command : IRequest
+      {
+         public Guid Id { get; set; }
+      }
 
-        public class Handler : IRequestHandler<Command>
-        {
-            private readonly DataContext _context;
-            public Handler(DataContext context)
-            {
-                _context = context;
-            }
+      public class Handler : IRequestHandler<Command>
+      {
+         private readonly DataContext _context;
+         public Handler(DataContext context)
+         {
+            _context = context;
+         }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
-            {
-                var activity = await _context.Activities.FindAsync(request.Id);
+         public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+         {
+            var activity = await _context.Activities.FindAsync(request.Id);
 
-                if (activity == null)
-                    throw new Exception("Could not find activity");
+            if (activity == null)
+               throw new Errors.RestException(System.Net.HttpStatusCode.NotFound, new { activity = "Not found" });
 
-                _context.Remove(activity);
+            _context.Remove(activity);
 
-                var success = await _context.SaveChangesAsync() > 0;
+            var success = await _context.SaveChangesAsync() > 0;
 
-                if (success) return Unit.Value;
+            if (success) return Unit.Value;
 
-                throw new Exception("Problem saving changes");
-            }
-        }
-    }
+            throw new Exception("Problem saving changes");
+         }
+      }
+   }
 }
--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -7,26 +7,30 @@ using Persistence;
 
 namespace Application.Activities
 {
-    public class Details
-    {
-        public class Query : IRequest<Activity>
-        {
-            public Guid Id { get; set; }
-        }
+   public class Details
+   {
+      public class Query : IRequest<Activity>
+      {
+         public Guid Id { get; set; }
+      }
 
-        public class Handler : IRequestHandler<Query, Activity>
-        {
-            private readonly DataContext _context;
-            public Handler(DataContext context)
-            {
-                _context = context;
-            }
+      public class Handler : IRequestHandler<Query, Activity>
+      {
+         private readonly DataContext _context;
+         public Handler(DataContext context)
+         {
+            _context = context;
+         }
 
-            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
-            {
-                var activity = await _context.Activities.FindAsync(request.Id);
-                return activity; 
-            }
-        }
-    }
+         public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+         {
+            var activity = await _context.Activities.FindAsync(request.Id);
+
+            if (activity == null)
+               throw new Errors.RestException(System.Net.HttpStatusCode.NotFound, new { activity = "Not found" });
+
+            return activity;
+         }
+      }
+   }
 }
